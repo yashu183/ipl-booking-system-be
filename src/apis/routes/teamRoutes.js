da5716e73c8e1c147dd2d5fd204c2564b063be55
Yashu <cyashu2000@gmail.com>
2025-03-24 01:08:19 +0530

    change: Removed admin check for getAllTeams

--- src/apis/routes/teamRoutes.js
+++ src/apis/routes/teamRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getAllTeams } = require("../handlers/team.handler");
 const { verifyToken, isAdmin } = require("../../middlewares/authorizationHandler");
 
-router.get('/', verifyToken, isAdmin, getAllTeams);
+router.get('/', verifyToken, getAllTeams);
 
 module.exports = router;
